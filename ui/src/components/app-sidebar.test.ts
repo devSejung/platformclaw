6f69f25ab6a4780b44bbf55ceae2ad9fa8f0c6b5
devSejung <sejung5997@gmail.com>
2026-09-14 12:03:37 +0900

    test(ui): split child session sidebar cases

--- ui/src/components/app-sidebar.test.ts
+++ ui/src/components/app-sidebar.test.ts
@@ -15,6 +15,7 @@ import "../test-helpers/app-sidebar-cases/catalog-ownership.ts";
 import "../test-helpers/app-sidebar-cases/catalog-pages.ts";
 import "../test-helpers/app-sidebar-cases/child-sessions-cap.ts";
 import "../test-helpers/app-sidebar-cases/child-sessions.ts";
+import "../test-helpers/app-sidebar-cases/child-sessions-orphans.ts";
 import "../test-helpers/app-sidebar-cases/group-mutations.ts";
 import "../test-helpers/app-sidebar-cases/identity-menu.ts";
 import "../test-helpers/app-sidebar-cases/interactions.ts";
