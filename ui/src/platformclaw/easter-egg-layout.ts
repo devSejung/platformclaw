ceece271ce89da37f48cd90a7cc3cc590e1405a3
hyeonho.jung <kmo4744@gmail.com>
2026-09-20 01:09:56 +0900

    fix(platformclaw): remove stale baseball layout export

--- ui/src/platformclaw/easter-egg-layout.ts
+++ ui/src/platformclaw/easter-egg-layout.ts
@@ -64,7 +64,7 @@ export function resolveBaseballFieldLayout(
   };
 }
 
-export function baseballWorldScreenX(
+function baseballWorldScreenX(
   x: number,
   layout: BaseballFieldLayout,
   pitchProjection = false,
